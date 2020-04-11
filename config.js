23e0359a5af3ab460d2d45f48ae8d959c02f9511
Priyanka Patil <priyankapatil0896@gmail.com>
2020-04-11 14:55:20 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/reactReduxApp'
+    'mongoUrl' : 'mongodb://localhost:27017/reactReduxApp',
+    'facebook' : {
+        clientId: '665392050925925',
+        clientSecret: '8f7da2e1108967c39ccf1ab526a8d080'
+    }
 }
